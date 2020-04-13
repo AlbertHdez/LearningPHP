a8337d35f1794a10c4db3e6660a5c827e2fd4a98
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-04-13 14:04:20 -0500

    Array interations and associative

--- main.php
+++ main.php
@@ -1,36 +1,25 @@
 <?php
 
-        // Funciones para string
-        $mensaje = "Hoy voy a aprender mucho";
-        // Longitud
-        echo strlen($mensaje);
+        $frutas = array('Melocoton', 'Sandia', 'Manzana', 'Guayaba');
 
-        echo '<br>';
-        // Numero de palabras
-        echo str_word_count($mensaje);
-        echo '<br>';
-        // Reversa
-        echo strrev($mensaje);
-        echo '<br>';
-        // Encontrar Texto
-        echo strpos($mensaje, 'aprender');
-        echo '<br>';
-        // Remplazar Texto
-        echo str_replace('aprender', 'learn', $mensaje);
-        echo '<br>';
-        // Convertir en minuscula
-        echo strtolower($mensaje);
-        echo '<br>';
-        // Convertir en mayuscula
-        echo strtoupper($mensaje);
-        echo '<br>';
-        // Comparar cadenas
-        echo strcmp($mensaje, 'Hoy');
-        echo '<br>';
-        // Substraer cadenas
-        echo substr($mensaje, 10, 8);
-        echo '<br>';
-        // Remover espacios en blanco
-        echo trim("   Hola chuco");
+        print_r($frutas);
 
+        for($i = 0; $i < count($frutas); $i++){
+            echo '<br>';
+            echo $frutas[$i];
+        }
+
+        $crushes = array(
+            "Raquel" => 19,
+            "Aly" => 19,
+            "Andrea" => 19,
+            "Alondra" => 18,
+            "Martha" => 17
+        );
+
+        print_r($crushes);
+
+        foreach($crushes as $crush){
+            echo '<br>' . $crush;
+        }
 ?>
