1ddb81400181cb894e59ca6b1e30a85c73b099f7
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-04-13 13:04:54 -0500

    String functions

--- main.php
+++ main.php
@@ -1,7 +1,36 @@
 <?php
 
-        //Print arrays
+        // Funciones para string
+        $mensaje = "Hoy voy a aprender mucho";
+        // Longitud
+        echo strlen($mensaje);
 
-        print_r(array(4, 5, 6));
+        echo '<br>';
+        // Numero de palabras
+        echo str_word_count($mensaje);
+        echo '<br>';
+        // Reversa
+        echo strrev($mensaje);
+        echo '<br>';
+        // Encontrar Texto
+        echo strpos($mensaje, 'aprender');
+        echo '<br>';
+        // Remplazar Texto
+        echo str_replace('aprender', 'learn', $mensaje);
+        echo '<br>';
+        // Convertir en minuscula
+        echo strtolower($mensaje);
+        echo '<br>';
+        // Convertir en mayuscula
+        echo strtoupper($mensaje);
+        echo '<br>';
+        // Comparar cadenas
+        echo strcmp($mensaje, 'Hoy');
+        echo '<br>';
+        // Substraer cadenas
+        echo substr($mensaje, 10, 8);
+        echo '<br>';
+        // Remover espacios en blanco
+        echo trim("   Hola chuco");
 
 ?>
