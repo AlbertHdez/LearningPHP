df4d83aae54262335126f70f2305f53ec9bd2885
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-04-15 22:06:15 -0500

    Date functions

--- main.php
+++ main.php
@@ -1,16 +1,20 @@
 <?php
 
-       $num1 = 10;
-       $num2 = 20;
-
-       function multiply($a, $b){
-           if(gettype($a) != "integer" || gettype($b) != "integer"){
-                echo "Introduce solo numeros";
-                return null;
-           }
-            return $a * $b;
-       }
-
-       echo multiply($num1, '');
-       echo multiply($num1, $num2);
+       //Fechas
+       echo 'Fecha: ' . date('d');
+       echo '<br>';
+       echo 'Fecha: ' . date('D');
+       echo '<br>';
+       echo 'Fecha: ' . date('m');
+       echo '<br>';
+       echo 'Fecha: ' . date('M');
+       echo '<br>';
+       echo 'Fecha: ' . date('Y');
+       echo '<br>';
+       echo 'Fecha: ' . date('y');
+       echo '<br>';
+       echo 'Fecha: ' . date('H');
+       echo '<br>';
+       echo 'Fecha: ' . date('s');
+       echo '<br>';
 ?>
