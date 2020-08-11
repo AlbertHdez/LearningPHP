62b8869c06b196d13e79dca8340cec3df37e88ee
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-08-10 21:11:54 -0500

    Usign a image modifier package

--- index.php
+++ index.php
@@ -2,37 +2,11 @@
 
 require_once 'vendor/autoload.php';
 
-// DB conexion
-$conexion = new mysqli("localhost", "ADMIN", "Masis12031964", "notes");
-$conexion->query("SET NAMES 'utf8'");
+$img_path = 'image.jpg';
+$save_path = 'mod-image.jpg';
 
+$thumb =  new PHPThumb\GD($img_path);
+$thumb->resize(50, 50);
 
-// consult to pagination
-$consult = $conexion->query("SELECT COUNT(id) as 'total' FROM notes");
-$rows = $consult->fetch_object()->total;
-$number_elements_page = 2;
-// Make pagination
-$pagination = new Zebra_Pagination();
-
-// Number of elements to page
-$pagination->records($rows);
-
-// Number of elements per page
-$pagination->records_per_page($number_elements_page);
-
-$page = $pagination->get_page();
-
-$start = ($page - 1) * $number_elements_page;
-
-$sql = "SELECT * FROM notes LIMIT $start, $number_elements_page";
-
-$notes = $conexion->query($sql);
-
-echo '<link rel="stylesheet" href="vendor/stefangabos/zebra_pagination/public/css/zebra_pagination.css" type="text/css">';
-
-while($note = $notes->fetch_object()){
-		echo "<br><h3>{$note->title}</h3>";
-		echo "<p>{$note->description}</p> <br> <hr>";
-}
-
-$pagination->render();
+$thumb->show();
+$thumb->save($save_path);
